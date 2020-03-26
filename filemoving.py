e62b2a7dd8b49c1c11399a75da8bb142552981b4
amir tadros <amirtadros01@gmail.com>
2020-03-26 13:01:33 -0700

    Update filemoving.py

--- filemoving.py
+++ filemoving.py
@@ -14,24 +14,26 @@ PHOTOS = "/Users/" + USER + "/Downloads/Photos"
 PROGRAMS = "/Users/" + USER + "/Downloads/Programs"
 ZIPS = "/Users/" + USER + "/Downloads/zip"
 
-def setup():
-	#photos
-	if not exists(PHOTOS):
-		mkdir(PHOTOS)
-	#documents
-	if not exists(DOCUMENTS):
-		mkdir(DOCUMENTS)
-	#music
-	if not exists(MUSIC):
-		mkdir(MUSIC)
-	#programs
-	if not exists(PROGRAMS):
-		mkdir(PROGRAMS)
-	#zip
-	if not exists(ZIPS):
-		mkdir(ZIPS)
 
-	move_files()
+def setup():
+    # photos
+    if not exists(PHOTOS):
+        mkdir(PHOTOS)
+    # documents
+    if not exists(DOCUMENTS):
+        mkdir(DOCUMENTS)
+    # music
+    if not exists(MUSIC):
+        mkdir(MUSIC)
+    # programs
+    if not exists(PROGRAMS):
+        mkdir(PROGRAMS)
+    # zip
+    if not exists(ZIPS):
+        mkdir(ZIPS)
+
+    move_files()
+
 
 def download_wait():
     timeout = 30
@@ -47,33 +49,36 @@ def download_wait():
                 wait = True
         seconds += 1
 
+
 def move_files():
-	for filename in listdir(TRACK):
-		extension = splitext(filename)[-1].lower()
-		newDestination = src = TRACK + "/" + filename
+    for filename in listdir(TRACK):
+        extension = splitext(filename)[-1].lower()
+        newDestination = src = TRACK + "/" + filename
 
-		#pictures
-		if extension in {'.png', '.jpg', '.jpeg'}:
-			newDestination = PHOTOS + "/" + filename
-		#documents
-		elif extension in {'.doc', '.xls', '.xlsx', '.pdf', 'txt', '.docx', '.csv'}:
-			newDestination = DOCUMENTS + "/" + filename
-		#music
-		elif extension in {'.mp3', '.wav'}:
-			newDestination = MUSIC + "/" + filename
-		#programs
-		elif extension in {".exe", '.msi'}:
-			newDestination = PROGRAMS + "/" + filename
-		#zips
-		elif extension in {".zip"}:
-			newDestination = ZIPS + "/" + filename
+        # pictures
+        if extension in {'.png', '.jpg', '.jpeg'}:
+            newDestination = PHOTOS + "/" + filename
+        # documents
+        elif extension in {'.doc', '.xls', '.xlsx', '.pdf', 'txt', '.docx', '.csv'}:
+            newDestination = DOCUMENTS + "/" + filename
+        # music
+        elif extension in {'.mp3', '.wav'}:
+            newDestination = MUSIC + "/" + filename
+        # programs
+        elif extension in {".exe", '.msi'}:
+            newDestination = PROGRAMS + "/" + filename
+        # zips
+        elif extension in {".zip"}:
+            newDestination = ZIPS + "/" + filename
 
-		rename(src, newDestination)
+        rename(src, newDestination)
+
 
 class MyHandler(FileSystemEventHandler):
-	def on_created(self, event):
-		download_wait()
-		move_files()
+    def on_created(self, event):
+        download_wait()
+        move_files()
+
 
 setup()
 
